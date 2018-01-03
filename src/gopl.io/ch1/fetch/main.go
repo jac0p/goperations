e8dd1df8c5ce7c229526025395531d1af81952e6
jac0p <roland.vrg87@gmail.com>
2018-01-03 17:55:10 -0500

    Modified to copy body with 'io.Copy' instead of 'ioutil.ReadAll'. Added 'http://' prefix to incomplete URLs via 'strings.HasPrefix' method. Prints HTTP Status code.

--- src/gopl.io/ch1/fetch/main.go
+++ src/gopl.io/ch1/fetch/main.go
@@ -2,20 +2,26 @@ package main
 
 import (
     "fmt"
-    "io/ioutil"
+    "io"
+    // "io/ioutil"
     "net/http"
     "os"
+    "strings"
 )
 
 func main() {
     for _, url := range os.Args[1:] {
+        if strings.HasPrefix(url, "http://") == false {
+            url = "http://" + url
+        }
         resp, err := http.Get(url)
         if err != nil {
             fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
             os.Exit(1)
         }
         s := resp.Status
-        b, err := ioutil.ReadAll(resp.Body)
+        // b, err := ioutil.ReadAll(resp.Body)
+        b, err := io.Copy(os.Stdout, resp.Body)
         resp.Body.Close()
         if err != nil {
             fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
