5259ecd6250b147ae66ea30401385a479802a234
jac0p <roland.vrg87@gmail.com>
2018-01-03 23:10:39 -0500

    'check' function for panicking on errors. Checks if URL has 'https://' prefix. Creates output file and writes channel content to it.

--- src/gopl.io/ch1/fetchall/main.go
+++ src/gopl.io/ch1/fetchall/main.go
@@ -7,18 +7,34 @@ import (
     "net/http"
     "os"
     "time"
+    "strings"
 )
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func main() {
     start := time.Now()
     ch := make(chan string)
+    f, err := os.Create("/Users/jac0p/_Kompi/GO/src/gopl.io/ch1/fetchall/out")
+    check(err)
+
     for _, url := range os.Args[1:] {
+        if strings.HasPrefix(url, "https://") == false {
+            url = "https://" + url
+        }
         go fetch(url, ch) // start a goroutine
     }
     for range os.Args[1:] {
-        fmt.Println(<-ch) // receive from channel ch
+        // fmt.Println(<-ch) // receive from channel ch
+        f.WriteString(<-ch + "\n")
     }
-    fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+    // fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+    f.WriteString(fmt.Sprintf("%.2fs elapsed\n", time.Since(start).Seconds()))
+    f.Close()
 }
 
 func fetch(url string, ch chan<- string) {
