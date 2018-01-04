43cbcfe6aa37191fee1ce80b3d10296ff9283c29
jac0p <roland.vrg87@gmail.com>
2018-01-04 01:23:13 -0500

    Lissajous number of cycles parsed from URL parameter value "cycles"

--- src/gopl.io/ch1/server1/main.go
+++ src/gopl.io/ch1/server1/main.go
@@ -4,6 +4,21 @@ import (
     "fmt"
     "log"
     "net/http"
+    "image"
+    "image/color"
+    "image/gif"
+    "io"
+    "math"
+    "math/rand"
+    // "os"
+    "strconv"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+    whiteIndex = 0 // first color in palette
+    blackIndex = 1 // next color in palette
 )
 
 func main() {
@@ -13,4 +28,35 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+    qcycles, _ := strconv.Atoi(r.URL.Query()["cycles"][0])
+    lissajous(w, qcycles)
 }
+
+
+func lissajous(out io.Writer, qcycles int) {
+    const (
+        cycles  = 5     // number of complete x oscillator revolutions
+        res     = 0.001 // angular resolution
+        size    = 100   // image canvas covers [-size..+size]
+        nframes = 64    // number of animation frames
+        delay   = 8     // delay between frames in 10ms units
+    )
+    freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+    anim := gif.GIF{LoopCount: nframes}
+    phase := 0.0 // phase difference
+    for i := 0; i < nframes; i++ {
+        rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+        img := image.NewPaletted(rect, palette)
+        for t := 0.0; t < float64(qcycles)*2*math.Pi; t += res {
+        x := math.Sin(t)
+        y := math.Sin(t*freq + phase)
+        img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+            blackIndex)
+        }
+        phase += 0.1
+        anim.Delay = append(anim.Delay, delay)
+        anim.Image = append(anim.Image, img)
+    }
+    gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
