a58cb377cab15cc311a17dcb39ec981d9db62c51
jac0p <roland.vrg87@gmail.com>
2018-01-07 15:33:48 -0500

    Added TestCheckIfFile & TestWalkDir tests

--- src/jac0p/logstalker/logstalker_test.go
+++ src/jac0p/logstalker/logstalker_test.go
@@ -6,7 +6,10 @@ import (
     "testing"
     "jac0p/logstalker"
     "path/filepath"
+    // "sort"
 )
+
+// ch := make(chan string) // global channel 
 
 
 func TestSourceMatchChkParams(t *testing.T) {
@@ -43,7 +46,37 @@ func TestCreateTG(t *testing.T) {
     if _, err := os.Stat(tg); os.IsNotExist(err) {
         t.Errorf("CreateTG() didn't create: %s", tg)
     }
-
-
-
 }
+
+func TestCheckIfFile(t *testing.T) {
+    var tests = []struct {
+        input string
+        want bool
+    }{
+        {"/tmp/testing/afile", true},
+        {"/tmp/testing/bfile", true},
+        {"/tmp/testing/adir", false},
+    }
+
+    for _, test := range tests {
+        if logstalker.CheckIfFile(test.input) != test.want {
+            t.Errorf("CheckIfFile(%s) != %s", test.input, test.want)
+        }
+    }
+}
+
+func TestWalkDir(t *testing.T) {
+    input := "/tmp/testing/"
+    want := []string{"/tmp/testing/afile", "/tmp/testing/bfile", "/tmp/testing/adir/cfile", "/tmp/testing/adir/bdir/dfile"}
+    fl := logstalker.WalkDir(input)
+    // sort.Strings(fl)
+
+    // this test doesn't really tell all the truth
+    if len(fl) != len(want) {
+        t.Errorf("WalkDir(%q) = %v", input, fl)
+    }
+}
+
+
+
+
